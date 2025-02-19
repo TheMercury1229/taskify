5231eb4d5541f7320d091018a7d11986c4ac00e7
Hardik Gujrathi <gujarathihardik29@gmail.com>
2025-02-19 12:12:52 +0530

    change-for-dep

--- src/app/api/category/[id]/route.ts
+++ src/app/api/category/[id]/route.ts
@@ -4,31 +4,31 @@ import { verifyToken } from "@/lib/verifyToken";
 import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const { decoded, error, status } = verifyToken(req);
-  if (!decoded || typeof decoded !== "object" || !decoded.id) {
-    return NextResponse.json(
-      { error: "Invalid token payload" },
-      { status: 400 }
-    );
-  }
-  const param = await params;
-  const paramId = await param.id;
-  const category = await db.query.categories.findMany({
-    where: eq(categories.userId, Number(paramId)),
-  });
+// export async function GET(
+//   req: NextRequest,
+//   { params }: { params: { id: string } }
+// ) {
+//   const { decoded, error, status } = verifyToken(req);
+//   if (!decoded || typeof decoded !== "object" || !decoded.id) {
+//     return NextResponse.json(
+//       { error: "Invalid token payload" },
+//       { status: 400 }
+//     );
+//   }
+//   const param = await params;
+//   const paramId = await param.id;
+//   const category = await db.query.categories.findMany({
+//     where: eq(categories.userId, Number(paramId)),
+//   });
 
-  if (!category.length)
-    return NextResponse.json(
-      { error: "Category not found", success: false },
-      { status: 404 }
-    );
+//   if (!category.length)
+//     return NextResponse.json(
+//       { error: "Category not found", success: false },
+//       { status: 404 }
+//     );
 
-  return NextResponse.json(category[0]);
-}
+//   return NextResponse.json(category[0]);
+// }
 
 export async function PATCH(
   req: NextRequest,
